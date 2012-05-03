fd239ba8890e297d86058b0ddba7279885a0f737
cydparser <cydparser@gmail.com>
2012-05-03 08:19:50 -0700

    Add ido settings

--- home/emacs.d/tweaks/ido.el
+++ home/emacs.d/tweaks/ido.el
@@ -1,3 +1,7 @@
-(require 'ido)
-(setq ido-save-directory-list-file (expand-file-name "ido.last" user-cache-directory))
-(ido-mode t)
+(setq ido-create-new-buffer 'always
+      ido-enable-flex-matching t
+      ido-everywhere t
+      ido-ignore-extensions t
+      ido-save-directory-list-file (expand-file-name "ido.last" user-cache-directory))
+
+(ido-mode 1)
