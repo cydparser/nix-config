19bfd3975ddc37c46380af6018418f09a1532717
cydparser <cydparser@gmail.com>
2012-07-15 21:36:29 -0700

    Add yasnippet

---
+++ home/emacs.d/tweaks/yasnippet.el
@@ -0,0 +1,14 @@
+(require 'yasnippet)
+
+(setq yas/root-directory `(,(init-expand-file-name "snippets")
+                           ,(init-expand-file-name "snippets-local")))
+
+(dolist (dir yas/root-directory)
+  (make-directory dir t)
+  (yas/load-directory dir))
+
+(add-to-list hippie-expand-try-functions-list 'yas/hippie-try-expand)))
+
+(defun yasutils/uncapitalize (cap)
+  (concat (downcase (substring cap 0 1))
+          (substring cap 1)))
