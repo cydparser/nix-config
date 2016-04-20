b7c2d0272453ff5d000d5a346847c405f85cf613
cydparser <cydparser@gmail.com>
2016-04-20 23:52:58 +0000

    Modify xmonad config

--- home/xmonad/xmonad.hs
+++ home/xmonad/xmonad.hs
@@ -1,30 +1,38 @@
+module Main (main) where
+
+import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
-import System.IO
+import XMonad.Util.Run (spawnPipe)
 
+main :: IO ()
 main = do
-  xmproc <- spawnPipe "xmobar" -- xmproc <- spawnPipe "/path/to/xmobarbinary /home/jgoerzen/.xmobarrc"
-  xmonad $ defaultConfig
-    { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    , logHook = dynamicLogWithPP xmobarPP
-                    { ppOutput = hPutStrLn xmproc
-                    , ppTitle = xmobarColor "green" "" . shorten 50
-                    }
-    , modMask = mod4Mask     -- Rebind Mod to the Windows key
-    } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        ]
-
-myManageHook = composeAll
-  [ className =? "Gimp" --> doFloat ]
-
--- install scrot + xmobar
--- trayer xscreensaver
-
--- run xscreensaver-demo
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ def
+    { -- borderWidth = 0
+      focusedBorderColor = "black"
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , logHook = logHook' xmproc
+    , manageHook = manageDocks <+> manageHook' <+> manageHook def
+    , modMask = windowsKey
+    , terminal = "termite"
+    , workspaces = ["emacs", "web", "social"] ++ map show [4..9]
+    } `additionalKeys` keys
+  where
+    windowsKey = mod4Mask
+    logHook' xmproc =
+      dynamicLogWithPP xmobarPP
+        { ppOutput = hPutStrLn xmproc
+        , ppTitle  = xmobarColor "green" "" . shorten 50
+        }
+    manageHook' =
+      composeAll [ className =? "Gimp" --> doFloat ]
+    keys =
+      [ -- ((windowsKey .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+      ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+      -- , ((modMask, xK_Return), spawn (XMonad.terminal conf))
+      , ((0, xK_Print), spawn "scrot")
+        -- TODO add mod-o for switch other window
+      ]
