1d6a22e6721b06c4bd0d5b5099062f774f10b307
cydparser <cydparser@gmail.com>
2016-06-09 23:34:48 -0700

    Move intero layer into separate repo
    https://github.com/cydparser/spacemacs-intero

--- home/config/spacemacs.d/layers/intero/config.el
+++
@@ -1,7 +0,0 @@
-(configuration-layer/declare-layer 'haskell)
-
-(setq haskell-enable-ghc-mod-support nil
-      haskell-enable-ghci-ng-support nil)
-
-(spacemacs|defvar-company-backends intero-mode)
-(spacemacs|defvar-company-backends intero-repl-mode)
