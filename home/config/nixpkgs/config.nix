b721b9073db2911a35f8e73bbe20bcbad8ebc0fb
cydparser <cydparser@gmail.com>
2017-05-08 17:31:43 -0700

    Remove packageOverrides

--- home/config/nixpkgs/config.nix
+++ home/config/nixpkgs/config.nix
@@ -2,12 +2,4 @@ pkgs: {
 
   allowUnfree = true;
 
-  packageOverrides = super: let self = super.pkgs; in {
-    haskellPackages = super.haskellPackages.override {
-      overrides = self: super: {
-        intero-nix-shim = self.callPackage ~/src/intero-nix-shim {};
-      };
-    };
-  };
-
 }
