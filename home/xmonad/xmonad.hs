72a9c0de8f5d7aebe128f543a5a6818db4f45c19
cydparser <cydparser@gmail.com>
2018-04-08 21:12:52 -0700

    Improve XMonad config

--- home/xmonad/xmonad.hs
+++ home/xmonad/xmonad.hs
@@ -1,47 +1,67 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
 module Main (main) where
 
 import           System.Exit
 import           XMonad
+import           XMonad.Config.Desktop
 import           XMonad.Hooks.ManageDocks
+import           XMonad.Layout.NoBorders (noBorders)
+import           XMonad.Layout.ToggleLayouts (toggleLayouts)
+import           XMonad.Prompt
+import           XMonad.Prompt.ConfirmPrompt
 import qualified XMonad.StackSet as W
 import           XMonad.Util.EZConfig
 
 main :: IO ()
 main =
-  xmonad $ def
-    { focusedBorderColor = "gray"
+ xmonad $ desktopConfig
+    { focusedBorderColor = "#AAAAAA"
     , normalBorderColor = "black"
-    , layoutHook = avoidStruts layout
-    , manageHook = manageDocks <+> manageHook def
+    , layoutHook = desktopLayoutModifiers layouts
+    , manageHook = manageDocks <+> manageHook desktopConfig
     , modMask = mod4Mask -- windows key
-    , terminal = "termite"
+    , terminal = term
     } `removeKeysP` unbindings `additionalKeysP` bindings
 
-layout = tiled ||| Mirror tiled ||| Full
+term = "termite"
+
+layouts =
+  toggleLayouts (noBorders Full) (tiled ||| Mirror tiled)
   where
     tiled = Tall
       { tallNMaster        = 1
-      , tallRatioIncrement = 5/100
+      , tallRatioIncrement = 4/100
       , tallRatio          = 2/3
       }
 
+unbindings =
+  [ "M-j"
+  , "M-S-j"
+  , "M-S-k"
+  , "M-p"
+  , "M-S-q"
+  ]
+
 bindings =
   [ ("M-k",        kill)
-  , ("M-O",        windows W.focusUp)
+  , ("M-S-o",      windows W.focusUp)
   , ("M-o",        windows W.focusDown)
-  , ("M-q",        io exitSuccess)
-  , ("M-r",        spawn "xmonad --recompile && xmonad --restart")
-  , ("M-x",        spawn "dmenu_run")
+  , ("M-q",        confirmPrompt promptConfig "exit" (io exitSuccess))
+  , ("M-r",        spawn "xmonad --recompile && xmonad --restart && xmessage -center -timeout 3 Success")
+  , ("M-t",        spawn term)
+  , ("M-x",        spawn "dmenu_run -p '>>' -fn 'xft:inconsolata:size=16'")
   , ("M-<Down>",   withFocused (windows . W.sink))
   , ("M-<Left>",   sendMessage Shrink)
   , ("M-<Right>",  sendMessage Expand)
-  , ("M-?",        spawn "xprop | grep WM_CLASS")
+  , ("M-S-/",      spawn "xprop WM_CLASS WM_NAME WM_WINDOW_ROLE | xmessage -timeout 15 -file -")
   ]
+  where
+    promptConfig = def
+      { position          = Top
+      , alwaysHighlight   = True
+      , height            = 70
+      , promptBorderWidth = 3
+      , font              = "xft:inconsolata:size=16"
+      }
 
-unbindings =
-  [ "M-?"
-  , "M-j"
-  , "M-J"
-  , "M-K"
-  , "M-p"
-  ]
