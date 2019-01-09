833b07c0a46c12b6a6e6e8736798bdeb995f802f
cydparser <cydparser@gmail.com>
2019-01-08 21:55:21 -0800

    Ignore Xfce4-notifyd windows in xmonad

--- home/config/xmonad/xmonad.hs
+++ home/config/xmonad/xmonad.hs
@@ -115,7 +115,8 @@ bindings XConfig{..} =
            )
 
 manageHooks = composeAll
-  [ role =? "GtkFileChooserDialog" --> doRectFloat (paddedRect (1 % 4))
+  [ className =? "Xfce4-notifyd" --> doIgnore
+  , role =? "GtkFileChooserDialog" --> doRectFloat (paddedRect (1 % 4))
   ]
   where
     role = stringProperty "WM_WINDOW_ROLE"
