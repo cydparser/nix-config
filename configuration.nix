e7d15f8fcf924ddf7c5875d52538e48ea01fd8bf
cydparser <cydparser@gmail.com>
2021-02-28 07:57:05 -0800

    Copy recommendedSysctlSettings

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,18 @@
     cleanTmpDir = true;
     supportedFilesystems = [ "ecryptfs" ];
     tmpOnTmpfs = true;
+
+    # Copied from virtualisation.lxd.recommendedSysctlSettings
+    kernel.sysctl = {
+      "fs.inotify.max_queued_events" = 1048576;
+      "fs.inotify.max_user_instances" = 1048576;
+      "fs.inotify.max_user_watches" = 1048576;
+      "vm.max_map_count" = 262144;
+      "kernel.dmesg_restrict" = 1;
+      "net.ipv4.neigh.default.gc_thresh3" = 8192;
+      "net.ipv6.neigh.default.gc_thresh3" = 8192;
+      "kernel.keys.maxkeys" = 2000;
+    };
   };
 
   console = {
