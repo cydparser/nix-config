66d4ec9b41f8a8acba3e72a224d861036cbfe584
cydparser <cydparser@gmail.com>
2021-07-18 15:41:06 -0700

    Disable custom Iosevka for now (broken)

--- home/config/nixpkgs/home/configuration.nix
+++ home/config/nixpkgs/home/configuration.nix
@@ -73,9 +73,7 @@ with lib;
       unzip
       zsh
     ] ++ optionals cfg.fonts [
-      (iosevka-with "terminal" ./iosevka-terminal.toml)
     ] ++ optionals (cfg.fonts && cfg.gui) [
-      (iosevka-with "haskell" ./iosevka-haskell.toml)
     ] ++ optionals cfg.gui [
       firefox-bin
       google-chrome
