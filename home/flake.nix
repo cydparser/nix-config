13d6224259daf514e75f15ff950a66fa5b288236
cydparser <cydparser@gmail.com>
2023-12-25 23:58:26 -0800

    Expose overlays

--- home/flake.nix
+++ home/flake.nix
@@ -60,56 +60,56 @@
       };
     };
 
+    overlay = system: (self: super:
+      {
+        alejandra = inputs.alejandra.packages.${system}.default;
+
+        merriam-webster-1913 = self.stdenv.mkDerivation {
+          name = "merriam-webster-1913";
+          sourceRoot = ".";
+          src = self.fetchurl {
+            url = "https://s3.amazonaws.com/jsomers/dictionary.zip";
+            sha256 = "09y673q5v46ps72pnm1jz0jx2bcyfbsmzw3f2x9y9s99k3yf79ps";
+          };
+          nativeBuildInputs = [self.unzip];
+          installPhase = ''
+            mkdir -p "$out/dic"
+            tar -C "$out/dic" -xf dictionary/stardict-dictd-web1913-2.4.2.tar.bz2
+          '';
+        };
+
+        sdcv = self.symlinkJoin {
+          name = "sdcv";
+          paths = [super.sdcv];
+          buildInputs = [self.makeWrapper];
+          postBuild = ''
+            wrapProgram $out/bin/sdcv \
+              --set STARDICT_DATA_DIR "${self.merriam-webster-1913}"
+          '';
+        };
+
+        statix = inputs.statix.packages.${system}.statix;
+
+        xmllint = self.libxml2;
+      }
+      // super.lib.attrsets.genAttrs [
+        "cabal-fmt"
+        "eventlog2html"
+        "ghc-events"
+        "ghc-events-analyze"
+        "lentil"
+        "profiteur"
+        "stylish-haskell"
+        "threadscope"
+      ] (name: self.haskell.lib.justStaticExecutables self.haskellPackages.${name}));
+
     username = "cyd";
   in
     flake-utils.lib.eachSystem systems (
       system: let
-        overlay = self: super:
-          {
-            alejandra = inputs.alejandra.packages.${system}.default;
-
-            merriam-webster-1913 = self.stdenv.mkDerivation {
-              name = "merriam-webster-1913";
-              sourceRoot = ".";
-              src = self.fetchurl {
-                url = "https://s3.amazonaws.com/jsomers/dictionary.zip";
-                sha256 = "09y673q5v46ps72pnm1jz0jx2bcyfbsmzw3f2x9y9s99k3yf79ps";
-              };
-              nativeBuildInputs = [self.unzip];
-              installPhase = ''
-                mkdir -p "$out/dic"
-                tar -C "$out/dic" -xf dictionary/stardict-dictd-web1913-2.4.2.tar.bz2
-              '';
-            };
-
-            sdcv = self.symlinkJoin {
-              name = "sdcv";
-              paths = [super.sdcv];
-              buildInputs = [self.makeWrapper];
-              postBuild = ''
-                wrapProgram $out/bin/sdcv \
-                  --set STARDICT_DATA_DIR "${self.merriam-webster-1913}"
-              '';
-            };
-
-            statix = inputs.statix.packages.${system}.statix;
-
-            xmllint = self.libxml2;
-          }
-          // super.lib.attrsets.genAttrs [
-            "cabal-fmt"
-            "eventlog2html"
-            "ghc-events"
-            "ghc-events-analyze"
-            "lentil"
-            "profiteur"
-            "stylish-haskell"
-            "threadscope"
-          ] (name: self.haskell.lib.justStaticExecutables self.haskellPackages.${name});
-
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [overlay];
+          overlays = [(overlay system)];
         };
 
         lib = pkgs.lib;
@@ -147,5 +147,8 @@
 
       nixosModules =
         nixpkgs.lib.attrsets.genAttrs nixosHosts home-manager-module;
+
+      overlays =
+        nixpkgs.lib.attrsets.genAttrs systems overlay;
     };
 }
