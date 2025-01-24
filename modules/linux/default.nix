80585715edd0924fae86e5a0934befaeb7f7cba1
cydparser <cydparser@gmail.com>
2025-01-23 17:17:24 -0800

    Add Linux modules

---
+++ modules/linux/default.nix
@@ -0,0 +1,137 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./desktop.nix
+  ];
+
+  options.nix-config =
+    let
+      inherit (lib) types;
+    in
+    {
+      audio.enable = {
+        type = types.bool;
+        default = true;
+      };
+    };
+
+  config =
+    let
+      cfg = config.nix-config;
+    in
+    {
+      boot = {
+        kernel.sysctl = {
+          # Copied from virtualisation.lxd.recommendedSysctlSettings
+          "fs.inotify.max_queued_events" = 1048576;
+          "fs.inotify.max_user_instances" = 1048576;
+          "fs.inotify.max_user_watches" = 1048576;
+          "vm.max_map_count" = 262144;
+          "kernel.dmesg_restrict" = 1;
+          "net.ipv4.neigh.default.gc_thresh3" = 8192;
+          "net.ipv6.neigh.default.gc_thresh3" = 8192;
+          "kernel.keys.maxkeys" = 2000;
+
+          "kernel.sysrq" = builtins.foldl' builtins.bitOr 0 [
+            2 # console logging
+            8 # dumps
+            16 # sync
+            128 # reboot/poweroff
+          ];
+        };
+
+        tmp = {
+          cleanOnBoot = true;
+        };
+      };
+
+      console = {
+        earlySetup = true;
+        font = "ter-232n";
+        keyMap = "dvorak";
+
+        packages = [
+          pkgs.terminus_font
+        ];
+
+        useXkbConfig = true;
+      };
+
+      fileSystems."/".options = [ "noatime" ];
+
+      fonts = {
+        fontconfig = {
+          allowBitmaps = false;
+          subpixel.rgba = "none";
+        };
+      };
+
+      hardware = {
+        graphics.enable32Bit = true;
+
+        pulseaudio = {
+          enable = cfg.audio.enable;
+          support32Bit = true;
+        };
+      };
+
+      networking.firewall.allowPing = false;
+
+      programs.ssh.startAgent = true;
+
+      security = {
+        pam = {
+          loginLimits = [
+            {
+              domain = "*";
+              type = "-";
+              item = "nofile";
+              value = "524288";
+            }
+          ];
+        };
+
+        rtkit.enable = cfg.audio.enable;
+      };
+
+      services = {
+        acpid.enable = true;
+
+        dictd = {
+          enable = true;
+          DBs = with pkgs.dictdDBs; [
+            wiktionary
+            wordnet
+          ];
+        };
+
+        libinput = {
+          enable = true;
+
+          mouse = {
+            naturalScrolling = true;
+          };
+
+          touchpad = {
+            accelSpeed = "1.0";
+            disableWhileTyping = true;
+            naturalScrolling = true;
+          };
+        };
+
+        ntp.enable = true;
+
+        pipewire = {
+          enable = cfg.audio.enable;
+          alsa.enable = true;
+          alsa.support32Bit = true;
+          pulse.enable = true;
+        };
+      };
+    };
+}
---
+++ modules/linux/desktop.nix
@@ -0,0 +1,65 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options =
+    let
+      inherit (lib) types;
+    in
+    {
+      windowManager = {
+        type = types.nullOr (types.enum [ "xmonad" ]);
+        default = "xmonad";
+      };
+    };
+
+  config =
+    let
+      inherit (lib) modules;
+
+      cfg = config.nix-config.desktop;
+
+      enableXserver = cfg.windowManager == "xmonad";
+    in
+    modules.mkMerge [
+      {
+        services = {
+          xserver = {
+            enable = enableXserver;
+            autoRepeatDelay = 325;
+            autoRepeatInterval = 45;
+
+            xkb = {
+              layout = "us,us";
+              variant = "dvorak,";
+              # NB: Pressing Win+space is not the same as `setxkbmap us`.
+              options = "caps:ctrl_modifier,ctrl:ralt_rctrl,grp:win_space_toggle";
+            };
+          };
+        };
+      }
+      (modules.mkIf (cfg.windowManager == "xmonad") {
+        services = {
+          displayManager.defaultSession = "none+xmonad";
+
+          environment.systemPackages = with pkgs; [
+            dmenu
+          ];
+
+          xserver = {
+            displayManager.lightdm.enable = true;
+
+            windowManager = {
+              xmonad = {
+                enable = true;
+                enableContribAndExtras = true;
+              };
+            };
+          };
+        };
+      })
+    ];
+}
