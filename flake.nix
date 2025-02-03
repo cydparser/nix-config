e8c7889bb09619296bf16d4b042f3fb179e5b538
cydparser <cydparser@gmail.com>
2025-02-03 14:39:02 -0800

    Simplify lock

--- flake.nix
+++ flake.nix
@@ -33,7 +33,10 @@
 
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     systems.url = "github:nix-systems/default";
   };
