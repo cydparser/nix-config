3dccd8bb8d6aa2ccedf73b894531842ae4dd760b
cydparser <cydparser@gmail.com>
2025-05-12 12:12:09 -0700

    Add temporary fix

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -29,6 +29,27 @@ in
     package = pkgs.nix-ld-rs;
   };
 
+  # TODO: Remove after fix: https://github.com/nix-community/NixOS-WSL/issues/650
+  # Copied from https://github.com/nix-community/NixOS-WSL/blob/10154ce2b21236653e40b87c45079792ae8ca950/modules/wsl-distro.nix
+  # Prevent systemd from mounting a tmpfs over the runtime dir (and thus hiding the wayland socket)
+  systemd.services."user-runtime-dir@" = {
+    overrideStrategy = "asDropin";
+    serviceConfig.ExecStart =
+      let
+        wrapped = pkgs.writeShellScript "user-runtime-dir-start-wrapped" ''
+          if [ -d "/run/user/$1" ]; then
+            exit 0
+          else
+            ${config.systemd.package}/lib/systemd/systemd-user-runtime-dir start "$1"
+          fi
+        '';
+      in
+      [
+        "" # unset old value
+        "${wrapped} %i"
+      ];
+  };
+
   wsl = {
     enable = true;
     defaultUser = cfg.username;
